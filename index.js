e8734ad21a71b2d5dfb41b5c92d62b32f448725b
claudivan1ete <aluno1ete@gmail.com>
2023-05-12 18:55:14 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const livros = [
+    {
+        id:1,
+        titulo: "person jackson e os olimpianos",
+        subtitulo: "o ladrão de raios",
+        autor: "rick riordan",
+        post: "./assets/img/livro3.webp"
+    }, 
+    {
+        id:1,
+        titulo: "person jackson e os olimpianos",
+        subtitulo: "o ladrão de raios",
+        autor: "rick riordan",
+        post: "./assets/img/livro3.webp"
+    }, 
+    {
+        id:1,
+        titulo: "person jackson e os olimpianos",
+        subtitulo: "o ladrão de raios",
+        autor: "rick riordan",
+        post: "./assets/img/livro3.webp"
+    }, 
+    {
+        id:1,
+        titulo: "person jackson e os olimpianos",
+        subtitulo: "o ladrão de raios",
+        autor: "rick riordan",
+        post: "./assets/img/livro3.webp"
+    }
+]
+
+
+export default livros;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import livros from "../index.js"
+const Livros = livros
+
+function render(Livros){
+    if()
+}
+render(Livros);
